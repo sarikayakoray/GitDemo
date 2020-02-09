19847c9ea02704ec800bad7162673d6dc3981748
sarik <sarik@DESKTOP-NBT0B2D>
2020-02-08 20:28:24 -0500

    this is to show how git works after making changes.

--- GitDemo/src/main/java/com/git/GitComment.java
+++ GitDemo/src/main/java/com/git/GitComment.java
@@ -1,10 +1,15 @@
 package com.git;
 
-public class GitComment {
+public class GitComment {// this is where the class begins
 
-	public static void main(String[] args) {
+	// EVERYTHING INSIDE THE MAIN METHOD RUNS
+	
+	public static void main(String[] args) { // the main method starts from here
 		// TODO Auto-generated method stub
+		
+		//this is the java method that will on out console
+		System.out.println("This is gonna show in Github once I push");
 
-	}
+	} // the main method ends here
 
-}
+}// this is where the class ends
